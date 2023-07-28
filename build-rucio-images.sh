32e0f0f84db3fd62ef7ef42e899f0705607effb3
Yuyi Guo <yuyi@fnal.gov>
2023-07-28 16:34:07 +0000

    add remote pdb

--- build-rucio-images.sh
+++ build-rucio-images.sh
@@ -3,16 +3,16 @@
 set -e
 
 export RUCIO_VERSION=1.29.12
-export CMS_VERSION=${RUCIO_VERSION}.yuyi1
+export CMS_VERSION=${RUCIO_VERSION}.yuyi3
 export CMS_TAG=cms_1_29_4
 
 export HARBOR=yuyiguo
 
-podman build --build-arg RUCIO_VERSION=$RUCIO_VERSION -f docker/rucio-server/Dockerfile -t $HARBOR/rucio-server:release-$CMS_VERSION .
-podman push $HARBOR/rucio-server:release-$CMS_VERSION
+#sudo podman build --build-arg RUCIO_VERSION=$RUCIO_VERSION -f docker/rucio-server/Dockerfile -t $HARBOR/rucio-server:release-$CMS_VERSION .
+#sudo podman push $HARBOR/rucio-server:release-$CMS_VERSION
 
-podman build --build-arg RUCIO_VERSION=$RUCIO_VERSION -f docker/rucio-daemons/Dockerfile -t $HARBOR/rucio-daemons:release-$CMS_VERSION .
-podman push $HARBOR/rucio-daemons:release-$CMS_VERSION
+sudo docker build --build-arg RUCIO_VERSION=$RUCIO_VERSION -f docker/rucio-daemons/Dockerfile -t $HARBOR/rucio-daemons:release-$CMS_VERSION .
+sudo docker push $HARBOR/rucio-daemons:release-$CMS_VERSION
 
 #podman build --build-arg RUCIO_VERSION=$RUCIO_VERSION -f docker/rucio-probes/Dockerfile -t $HARBOR/rucio-probes:release-$CMS_VERSION .
 #podman push $HARBOR/rucio-probes:release-$CMS_VERSION
