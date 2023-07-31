2a8eda130b2a5bc5a8428272945ccc03e53b9106
Yuyi Guo <yuyi@fnal.gov>
2023-07-31 22:02:08 +0000

    build for automatix: ca certs and sleep.

--- build-rucio-images.sh
+++ build-rucio-images.sh
@@ -3,7 +3,7 @@
 set -e
 
 export RUCIO_VERSION=1.29.12
-export CMS_VERSION=${RUCIO_VERSION}.yuyi3
+export CMS_VERSION=${RUCIO_VERSION}.yuyi-ca-sleep4
 export CMS_TAG=cms_1_29_4
 
 export HARBOR=yuyiguo
@@ -11,7 +11,7 @@ export HARBOR=yuyiguo
 #sudo podman build --build-arg RUCIO_VERSION=$RUCIO_VERSION -f docker/rucio-server/Dockerfile -t $HARBOR/rucio-server:release-$CMS_VERSION .
 #sudo podman push $HARBOR/rucio-server:release-$CMS_VERSION
 
-sudo docker build --build-arg RUCIO_VERSION=$RUCIO_VERSION -f docker/rucio-daemons/Dockerfile -t $HARBOR/rucio-daemons:release-$CMS_VERSION .
+sudo docker build --no-cache --build-arg RUCIO_VERSION=$RUCIO_VERSION -f docker/rucio-daemons/Dockerfile -t $HARBOR/rucio-daemons:release-$CMS_VERSION .
 sudo docker push $HARBOR/rucio-daemons:release-$CMS_VERSION
 
 #podman build --build-arg RUCIO_VERSION=$RUCIO_VERSION -f docker/rucio-probes/Dockerfile -t $HARBOR/rucio-probes:release-$CMS_VERSION .
